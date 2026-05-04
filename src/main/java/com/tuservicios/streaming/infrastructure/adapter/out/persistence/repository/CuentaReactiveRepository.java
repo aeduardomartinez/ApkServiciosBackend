aa9444a4cba25f12d0bd7badb6dc0c90c5e9f181
Andres Martinez <andres.ok30@hotmail.com>
2026-05-04 15:46:05 -0500

    Nuevos cambio 2

--- src/main/java/com/tuservicios/streaming/infrastructure/adapter/out/persistence/repository/CuentaReactiveRepository.java
+++ src/main/java/com/tuservicios/streaming/infrastructure/adapter/out/persistence/repository/CuentaReactiveRepository.java
@@ -1,6 +1,5 @@
 package com.tuservicios.streaming.infrastructure.adapter.out.persistence.repository;
 
-import org.springframework.data.annotation.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
@@ -17,7 +16,6 @@ public interface CuentaReactiveRepository extends R2dbcRepository<CuentaEntity, 
    Flux<CuentaEntity> findByCorreoPrincipal(String correo);
    Flux<CuentaEntity> findByServicioId(Long servicioId);
 
-   @Modifying
    @Query("UPDATE cuentas SET correo_principal = :correo WHERE id = :id")
    Mono<Integer> updateCorreoPrincipal(Long id, String correo);
 
